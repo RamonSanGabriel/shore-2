0846d9c7778a02d4a5faec97a41a090322119d9b
RamonSanGabriel <jeshero11@gmail.com>
2025-02-27 00:15:18 +0800

    menu icons

--- client/src/data/menu-icons.jsx
+++ client/src/data/menu-icons.jsx
@@ -1,4 +1,7 @@
-import { MdHome } from 'react-icons/md';
+import { MdHome, MdMiscellaneousServices } from 'react-icons/md';
+import { ImAccessibility } from 'react-icons/im';
+import { TfiGallery } from 'react-icons/tfi';
+import { RiContactsBook3Fill } from 'react-icons/ri';
 
 export const menuIcons = [
   {
@@ -8,22 +11,22 @@ export const menuIcons = [
   },
   {
     id: 2,
-    icon: MdHome,
+    icon: ImAccessibility,
     label: 'About',
   },
   {
     id: 3,
-    icon: MdHome,
+    icon: MdMiscellaneousServices,
     label: 'Services',
   },
   {
     id: 4,
-    icon: MdHome,
+    icon: TfiGallery,
     label: 'Gallery',
   },
   {
     id: 5,
-    icon: MdHome,
+    icon: RiContactsBook3Fill,
     label: 'Contacts',
   },
 ];
