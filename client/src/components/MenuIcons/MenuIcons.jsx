be89b5d21da5c426ee581dca8e6153fb9a2996b1
RamonSanGabriel <jeshero11@gmail.com>
2025-02-27 00:29:49 +0800

    navlink menu icons

--- client/src/components/MenuIcons/MenuIcons.jsx
+++ client/src/components/MenuIcons/MenuIcons.jsx
@@ -1,16 +1,24 @@
 import css from './MenuIcons.module.css';
 import { menuIcons } from '../../data/menu-icons';
+import { NavLink } from 'react-router-dom';
 
 const MenuIcons = () => {
   return (
     <div className={css.menuIconsWrapper}>
       <ul className={css.menuList}>
-        {menuIcons.map(({ id, icon: Icon, label }) => (
+        {menuIcons.map(({ id, icon: Icon, label, path }) => (
           <div key={id}>
-            <li className={css.menuListItems}>
-              <Icon className={css.menuIcons} />
-            </li>
-            <p>{label}</p>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? css.navLink : css.navListName
+              }
+              to={path}
+            >
+              <li className={css.menuListItems}>
+                <Icon className={css.menuIcons} />
+              </li>
+              <p>{label}</p>
+            </NavLink>
           </div>
         ))}
       </ul>
--- client/src/data/menu-icons.jsx
+++ client/src/data/menu-icons.jsx
@@ -8,25 +8,30 @@ export const menuIcons = [
     id: 1,
     icon: MdHome,
     label: 'Home',
+    path: '/',
   },
   {
     id: 2,
     icon: ImAccessibility,
     label: 'About',
+    path: '/about',
   },
   {
     id: 3,
     icon: MdMiscellaneousServices,
     label: 'Services',
+    path: '/services',
   },
   {
     id: 4,
     icon: TfiGallery,
     label: 'Gallery',
+    path: '/gallery',
   },
   {
     id: 5,
     icon: RiContactsBook3Fill,
     label: 'Contacts',
+    path: '/contacts',
   },
 ];
