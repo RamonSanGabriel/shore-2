0ae42a639001232d33d366c5a85cec829925195d
RamonSanGabriel <jeshero11@gmail.com>
2025-03-01 01:02:00 +0800

    scroll animation

--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -2,6 +2,7 @@ import { gallery } from '../../data/gallery';
 import css from './Gallery.module.css';
 
 const Gallery = () => {
+  const { id, title, image, description } = gallery;
   return (
     <div className={css.galleryWrapper}>
       <ul className={css.galleryList}>
@@ -14,6 +15,13 @@ const Gallery = () => {
             </li>
           </div>
         ))}
+        <div className={css.galleryListContainer} key={id}>
+          <li className={css.galleryItems}>
+            <h3>{title}</h3>
+            <img src={image} alt={title} />
+            <p>{description}</p>
+          </li>
+        </div>
       </ul>
     </div>
   );
