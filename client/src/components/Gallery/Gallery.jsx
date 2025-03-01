87515e4fec26cbd6cb9a52fc939748d17697e3fa
RamonSanGabriel <jeshero11@gmail.com>
2025-03-01 10:20:38 +0800

    animation effect

--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -15,13 +15,6 @@ const Gallery = () => {
             </li>
           </div>
         ))}
-        <div className={css.galleryListContainer} key={id}>
-          <li className={css.galleryItems}>
-            <h3>{title}</h3>
-            <img src={image} alt={title} />
-            <p>{description}</p>
-          </li>
-        </div>
       </ul>
     </div>
   );
