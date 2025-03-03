1469e28611b907dd090cf8c46108c0de89c4c546
RamonSanGabriel <jeshero11@gmail.com>
2025-03-03 14:39:58 +0800

    modal

--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -5,36 +5,42 @@ import Modal from '../Modal/Modal';
 
 const Gallery = () => {
   const [modal, setModal] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(null);
   const [modalClose, setModalClose] = useState(true);
 
-  const handleOpenModal = () => {
+  const { id } = gallery;
+
+  const handleOpenModal = (id) => {
+    setCurrentIndex(id);
     setModal(true);
   };
+  console.log(handleOpenModal);
 
   const handleModalClose = (value) => {
-    setModalClose(false);
-    setModal(value);
+    setModal(false);
+    setCurrentIndex(value);
   };
 
   return (
     <div className={css.galleryWrapper}>
       {modal ? (
-        <Modal
-          setModal={setModal}
-          onClose={handleModalClose}
-          modalClose={modalClose}
-        />
+        <Modal onClose={handleModalClose} id={id} />
       ) : (
         <ul className={css.galleryList}>
-          {gallery.map(({ id, title, image, description }) => (
-            <div className={css.galleryListContainer} key={id}>
-              <li className={css.galleryItems}>
-                <h3 className={css.galleryTitle}>{title}</h3>
-                <img src={image} alt={title} onClick={handleOpenModal} />
-                <p>{description}</p>
-              </li>
-            </div>
-          ))}
+          {gallery &&
+            gallery.map(({ id, title, image, description }) => (
+              <div className={css.galleryListContainer} key={id}>
+                <li className={css.galleryItems}>
+                  <h3 className={css.galleryTitle}>{title}</h3>
+                  <img
+                    src={image}
+                    alt={title}
+                    onClick={() => handleOpenModal(id)}
+                  />
+                  <p>{description}</p>
+                </li>
+              </div>
+            ))}
         </ul>
       )}
     </div>
--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,7 +1,15 @@
 import css from './Modal.module.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import { gallery } from '../../data/gallery';
+import gallery1 from '../../images/gallery/img1.jpg';
 
-const Modal = ({ onClose, showModal, modalClose }) => {
+const Modal = ({ onClose }) => {
+  const [currentImage, setCurrentImage] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const { image, id, title } = gallery;
+
   // const [modalClose, setModalClose] = useState(true);
   // useEffect(() => {
   //   if (showModal) document.body.style.overflow = 'hidden';
@@ -12,17 +20,25 @@ const Modal = ({ onClose, showModal, modalClose }) => {
   // }, [showModal]);
 
   return (
-    // modalClose && (
-    <div className={css.modal}>
-      <button onClick={() => onClose()}>&times;</button>
-      <div className={css.modalHeader}>
-        <h2>Modal</h2>
+    !isModalOpen && (
+      <div className={css.modal}>
+        <button onClick={() => onClose()}>&times;</button>
+        <div className={css.modalHeader}>
+          <h2>Modal</h2>
+        </div>
+        <div className={css.modalBody}>
+          <p>Modal content</p>
+          <img src={gallery1} />
+          {/* `
+          <img
+            src="${image == null ? `https://placehold.co/400x600`:  `image`}"
+            alt="${title}"
+          />
+          ` */}
+          {/* <img src={image} /> */}
+        </div>
       </div>
-      <div className={css.modalBody}>
-        <p>Modal content</p>
-      </div>
-    </div>
-    // )
+    )
   );
 };
 
