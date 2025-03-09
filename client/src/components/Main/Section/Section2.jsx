24546bc116d52fd87e7c5e73aa929957069aa301
RamonSanGabriel <jeshero11@gmail.com>
2025-03-10 00:02:21 +0800

    removed section2 modal

--- client/src/components/Main/Section/Section2.jsx
+++ client/src/components/Main/Section/Section2.jsx
@@ -1,46 +1,18 @@
 import { useState } from 'react';
 import { features, storeys, vicinityMap } from '../../../data/main';
 import css from './Section.module.css';
-import Section2Modal from '../../Modal/section2Modal/section2Modal';
 
 const Section2 = () => {
-  const [openModal, setOpenModal] = useState(false);
-  const [onClose, setOnClose] = useState(true);
-  const [currentIndex, setCurrentIndex] = useState(0);
   const { titleMap, imageMap, contentMap, address, id } = vicinityMap;
-
-  const handleOpenModal = (id) => {
-    // setCurrentIndex(id - 1);
-    setOpenModal(true);
-  };
-
-  const handleModalClose = () => {
-    setOpenModal(false);
-  };
 
   return (
     <section className={css.section}>
-      {openModal ? (
-        <Section2Modal
-          // id={id}
-          image={imageMap}
-          // currentIndex={currentIndex}
-          setOpenModal={handleOpenModal}
-          // setCurrentIndex={setCurrentIndex}
-          onClose={handleModalClose}
-        />
-      ) : (
-        <div>
-          <h2 className={css.title}>{titleMap}</h2>
-          <address className={css.address}>{address}</address>
-          <img
-            className={(css.facade, css.facadeMap)}
-            onClick={handleOpenModal}
-            src={imageMap}
-          />
-          <p className={(css.content, css.contentMap)}>{contentMap}</p>
-        </div>
-      )}
+      <div>
+        <h2 className={css.title}>{titleMap}</h2>
+        <address className={css.address}>{address}</address>
+        <img className={(css.facade, css.facadeMap)} src={imageMap} />
+        <p className={(css.content, css.contentMap)}>{contentMap}</p>
+      </div>
     </section>
   );
 };
