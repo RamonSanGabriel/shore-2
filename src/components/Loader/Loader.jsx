c50a3b7dafce8e4199c81c14dbe085566d557e5c
RamonSanGabriel <jeshero11@gmail.com>
2025-03-10 23:38:23 +0800

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,13 +1,11 @@
 // import { RotatingLines } from 'react-loader-spinner';
 import { BeatLoader } from 'react-spinners';
-
 import css from './Loader.module.css';
 
 const Loader = () => {
   return (
     <div className={css.loaderContainer}>
-      {/* <h2>Loading...</h2> */}
-      <BeatLoader color="#3974d8" size={20} />
+      <BeatLoader color="#3974d8" size={30} />
     </div>
   );
 };
